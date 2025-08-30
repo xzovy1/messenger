5fc29c3064175acfc242e8fa71de5a2a295a9df8
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-08-30 09:07:08 -0600

    remove package.json & package-lock.json from root folder. add deps to backend folder

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,26 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-const session = require("express-session")
-
+const session = require("express-session");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(express.urlencoded({ extended: true }));
 
-const authRouter = require("./routes/auth")
-app.use("/", authRouter)
+const authRouter = require("./routes/auth");
+app.use("/", authRouter);
 
 const contactsRouter = require("./routes/contactsRouter");
-app.use("/contacts", contactsRouter)
+app.use("/contacts", contactsRouter);
 
 const messageRouter = require("./routes/messageRouter");
-app.use("/chat", messageRouter)
+app.use("/chat", messageRouter);
 
 const userRouter = require("./routes/userRouter");
-app.use("/user", userRouter)
+app.use("/user", userRouter);
 
 const PORT = process.env.PORT || PORT;
-app.listen(PORT, () => { console.log(`Listening on port ${PORT}`) });
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
