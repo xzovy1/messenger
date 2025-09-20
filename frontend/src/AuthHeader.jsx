c9d3f87806fba836a31fb4507e4234cf87669f5f
Caleb Steinwandt <caleb.steinwandt@gmail.com>
2025-09-20 07:44:15 -0600

    clicking logo at login or signup redirects to '/'

--- frontend/src/AuthHeader.jsx
+++ frontend/src/AuthHeader.jsx
@@ -1,12 +1,14 @@
 import styles from './public/app.module.css'
 import "./App.css";
 import messenger from './public/messenger.png'
+import { useNavigate } from 'react-router';
 const AuthHeader = () => {
+    const navigate = useNavigate();
     return (
-            <div className={styles.header}>
-                <h2>Odin Messenger</h2>
-                <div><img src={messenger} alt="" className="logo"/></div>
-            </div>
+        <div className={styles.header}>
+            <h2>Odin Messenger</h2>
+            <div onClick={() => { navigate('/') }}><img src={messenger} alt="" className="logo" /></div>
+        </div>
     )
 }
 
